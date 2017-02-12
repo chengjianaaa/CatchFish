30504f2ba89a7b2ebb694b6cf04d4dc610438c97
annasophie <annasophie.nagel@gmail.com>
2017-02-11 18:19:36 -0800

    Change order of input components on SearchForm Component, change labels to placeholders

--- client/src/components/SearchFormComponent.js
+++ client/src/components/SearchFormComponent.js
@@ -11,7 +11,6 @@ class SearchFormComponent extends Component {
       lastName: '',
       email: '',
       state: '',
-      country: '',
       phone: '',
       email: '',
       linkedIn: '',
@@ -28,7 +27,6 @@ class SearchFormComponent extends Component {
 
     this.setState(tempObj)
     console.log(this.state)
-
   };
 
   toggleOptions() {
@@ -40,16 +38,21 @@ class SearchFormComponent extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
-        <InputComponent handleChange={this.handleChange.bind(this)} property={'firstName'} label={"First Name: "}/>
-        <InputComponent handleChange={this.handleChange.bind(this)}  property={'lastName'} label={"Last Name: "}/>
-        <InputComponent handleChange={this.handleChange.bind(this)} property={'email'} label={"Email: "}/>
-        <InputComponent handleChange={this.handleChange.bind(this)} property={'phone'} label={"Phone: "}/>
 
-        <StateDropdownComponent handleChange={this.handleChange.bind(this)} property={'state'} label={"State: "}/>
+        <InputComponent handleChange={this.handleChange.bind(this)} property={'firstName'} placeholder={"First Name"}/>
 
-        <InputComponent handleChange={this.handleChange.bind(this)} property={'country'} label={"Country: "}/>
-        <InputComponent handleChange={this.handleChange.bind(this)} property={'linkedIn'} label={"linkedIn URL: "}/>
-        <InputComponent handleChange={this.handleChange.bind(this)} property={'facebook'} label={"Facebook URL: "}/>
+        <InputComponent handleChange={this.handleChange.bind(this)}  property={'lastName'} placeholder={"Last Name"}/>
+
+        <InputComponent handleChange={this.handleChange.bind(this)} property={'email'} placeholder={"Email"}/>
+
+        <InputComponent type="tel" handleChange={this.handleChange.bind(this)} property={'phone'} placeholder={"Phone"}/>
+
+        <InputComponent handleChange={this.handleChange.bind(this)} property={'linkedIn'} placeholder={"linkedIn URL"}/>
+
+        <InputComponent handleChange={this.handleChange.bind(this)} property={'facebook'} placeholder={"Facebook URL"}/>
+
+        <StateDropdownComponent handleChange={this.handleChange.bind(this)} property={'state'} placeholder={"State"}/>
+
       </form>
     )
   }
